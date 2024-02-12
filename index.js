3c59338d4ae8f1a8efb68ece5b9db86efc34f6a2
AnthonyBonanno <bonant12@gmail.com>
2024-02-13 01:44:10 +1100

    added validation and removed extra consts

--- index.js
+++ index.js
@@ -3,35 +3,44 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 const generateLogo = require('./lib/generateLogo');
-const fileName ='logo.svg';
+const fileName = 'output/logo.svg';
 
 // This is an array of prompts, each prompt stores a type, message, and name
 const prompts = [{
     type: 'input',
-    message: 'Please enter the text for your logo (it cannot exceed 3 letters):',
-    name: 'text'
+    message: 'Please enter the text for your logo (it cannot exceed 3 characters):',
+    name: 'text',
+    validate: function (input) {
+        if (input.length > 3) {
+            return 'The text for your logo cannot exceed 3 characters!';
+        } else {
+            return true;
+        }
+    }
 },
 {
-    type: 'input',
+    type: 'list',
     message: 'Please enter a hexadecimal number for the colour of your text:',
-    name: 'textColour'
+    name: 'textColour',
+    choices: ['Red', 'Green', 'Blue']
 },
 {
     type: 'list',
     message: 'Please choose a shape for you logo from the list below:',
-    name: 'shape'
+    name: 'shape',
     choices: ['Circle', 'Triangle', 'Square']
 },
 {
-    type: 'input',
+    type: 'list',
     message: 'Please enter a hexadecimal number for the colour of your shape:',
-    name: 'shapeColour'
+    name: 'shapeColour',
+    choices: ['Purple', 'Yellow', 'Orange']
 }];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, logoContent) { 
+function writeToFile(fileName, logoContent) {
     fs.writeFile(fileName, logoContent, (err) =>
-    err ? console.log(err) : console.log("Generated logo.svg"));
+        err ? console.log(err) : console.log("Generated logo.svg"));
 }
 
 // TODO: Create a function to initialize app
